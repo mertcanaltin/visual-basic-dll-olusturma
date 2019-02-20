09384b1c2e16d254fcd1fa77d93b86146b106c5c
Mert Can Altın <mertcanaltin@posta.mu.edu.tr>
2019-02-20 21:43:30 +0300

    Add files via upload

---
+++ DLLKullananAlanHesabiFormUygulamasi/Form1.vb
@@ -0,0 +1,17 @@
+﻿Imports alanHesabi ' Add reference İle eklediğim dll dosyamı import ettim
+Public Class Form1
+    Private Sub KareAlanHesaplaButton_Click(sender As Object, e As EventArgs) Handles KareAlanHesaplaButton.Click
+        Dim Alan_Hesap As New alanlar 'dll dosyasındaki class isminden yeni bir tanımlama yaptık 
+        Dim Kenar As Decimal = KareKenarTextBox.Text
+        Dim Sonuc As Decimal = Alan_Hesap.KareAlan(Kenar) 'tanımladığımız değşkeni kullanarak dll içindeki fonksiyona ulaştık
+        KareAlanTextBox.Text = Sonuc
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ucgen_hesap As New alanlar
+        Dim taban As Decimal = TextBox1.Text
+        Dim yuksek As Decimal = TextBox2.Text
+        Dim sonuc As Decimal = ucgen_hesap.UcgenAlan(taban, yuksek)
+        TextBox3.Text = sonuc
+    End Sub
+End Class
